aca4b00ff4c207a14c63e4c3fbea689bc69c9678
Smos Bois <simondeb1505@gmail.com>
2020-05-26 02:56:25 +0200

    Destruction!!1!

---
+++ BuildingWorld4/Assets/GrowUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowUp : MonoBehaviour
+{
+    public float growSpeed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.transform.localScale = Vector3.zero;
+        growSpeed = 0.2f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (this.transform.localScale.x < 1)
+            this.transform.localScale = new Vector3(Mathf.Lerp(this.transform.localScale.x, 1, growSpeed), Mathf.Lerp(this.transform.localScale.y, 1, growSpeed), Mathf.Lerp(this.transform.localScale.z, 1, growSpeed));
+
+        if (this.transform.localScale.x >= 1)
+            this.transform.localScale = new Vector3(1, 1, 1);
+    }
+}
---
+++ BuildingWorld4/Assets/Scripts/Gun.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 100f;
+    public GameObject wall;
+    public GameObject brokenWall;
+
+    public Camera fpsCam;
+    public Transform Player;
+    public ParticleSystem MuzzleFlash;
+
+    private float nextTimeToFire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Build();
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Build ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name + hit.point);
+
+            GameObject temp = Instantiate(wall, hit.point, Quaternion.FromToRotation(Vector3.up,hit.normal));
+        }
+    }
+
+    void Shoot()
+    {
+        MuzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log("Pew");
+            if(hit.transform.parent != null)
+            {
+                if (hit.transform.parent.gameObject.tag == "Breakable" && hit.transform.parent.localScale == new Vector3(1, 1, 1))
+                {
+                    Instantiate(brokenWall, hit.transform.parent.position, hit.transform.parent.rotation);
+                    Destroy(hit.transform.parent.gameObject);
+                }
+            }
+
+            if(hit.transform.GetComponent<Rigidbody>() != null)
+            {
+                hit.transform.GetComponent<Rigidbody>().AddForce(fpsCam.transform.forward * 3, ForceMode.Impulse);
+            }
+        }
+    }
+}
